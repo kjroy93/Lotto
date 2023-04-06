18bf1664ff4a67424d653edd9b5c517a2d2e8285
kjroy93 <kmarrero1993@gmail.com>
2023-04-06 21:58:14 +0200

    data structures for final analysis. Functions separated from the main code. Math for recomendations remaining

---
+++ DataAnalysis/data_functions.py
@@ -0,0 +1,164 @@
+# Functions
+
+import pandas as pd
+import numpy as np
+import pandas as pd
+import numpy as np
+from collections import Counter
+from decimal import Decimal, getcontext
+import data_functions
+getcontext().prec = 5
+np.set_printoptions(precision=5)
+
+def clean_df(df, columns_id, name):
+    df.columns = columns_id
+    df.columns.name = name
+    df.index.name = 'Draws'
+    return df
+
+def count_skips(df, list_numbers):
+    counts = {key: 0 for key in list_numbers}
+    for columns in df:
+        counter = 0
+        for i in reversed(df[columns]):
+            if not i:
+                counter += 1
+            else:
+                counts[columns] = counter
+                break       
+    return counts
+
+def max_output(df):
+    df = df.T
+    max_numbers = df.sort_values(by=df.columns[0], ascending=False)
+    return max_numbers
+
+def year_hits(database, df_with_numbers, numbers_quantity):
+    db_year = database['Dates'].dt.year
+    
+    def count_hits(year_number):
+        year, number = year_number
+        filtered = df_with_numbers.loc[(db_year == year) & (df_with_numbers == number).any(axis=1), :]
+        count = filtered.eq(number).sum().sum()
+        return pd.Series({'Year': year, 'Number': number, 'Count': int(count)})
+    
+    year_numbers = [(y, n) for y in range(db_year.min(), db_year.max()+1) for n in numbers_quantity]
+    year_history = pd.DataFrame(year_numbers, columns=['Year', 'Number'])
+    year_history = year_history.apply(count_hits, axis=1)
+    year_history = year_history.pivot_table(index='Year', columns='Number', values='Count', fill_value=0)
+    return year_history
+
+def get_hits(database, df_with_numbers, numbers_quantity):
+    hits = year_hits(database, df_with_numbers, numbers_quantity).sum().reset_index()
+    hits = hits.rename(columns = {'Number': 'Numbers', 'Count': 'Hits'}).set_index('Numbers').T
+    hits = hits.iloc[0].rename('Hits').to_frame()
+    hits.index.name = None
+    return hits.T
+
+def total_average_hits(database, hits, numbers, is_star=False, aprox=False):
+    divide = 2 if is_star else 5
+    average_hits = hits.apply(lambda hits: hits / len(database) / divide)
+    average_hits = average_hits.iloc[0].rename('Average').to_frame()
+    average_hits.index.name = None
+    if aprox:
+        return Decimal(average_hits['Average'].sum()) / Decimal(int(numbers.max())) + Decimal(0.001)
+    else:
+        return Decimal(average_hits['Average'].sum()) / Decimal(int(numbers.max()))
+
+def minimal_hits(database, hits, numbers, average, is_star=False, aprox=False):
+    min_hits = total_average_hits(database, hits, numbers, is_star, aprox)
+    return min_hits * Decimal(int(hits.iloc[0, 0])) / Decimal(float(average.iloc[0, 0]))
+
+def average_hits(database, hits_data, numbers, is_star=False):
+    divide = 2 if is_star else 5
+    hits_by_num = hits_data.apply(lambda hits: hits / len(database) / divide)
+    hits_by_num = hits_by_num.iloc[0].rename('Average').to_frame()
+    hits_by_num.index.name = None
+    averages = []
+    
+    for num in numbers:
+        avg = hits_by_num.loc[num]['Average'].sum()
+        averages.append(round(avg, 6))
+        
+    result = pd.DataFrame({'Numbers/Stars': numbers, 'Average_Hits': averages})
+    result = result.T
+    result.columns = result.iloc[0].astype(int)
+    result = result[1:]
+    return result
+
+def natural_rotation(database, hits, numbers, data_average, index_start, index_end, is_star=False, aprox=False):
+    average = total_average_hits(database, hits, numbers, is_star, aprox)
+    m_hits = minimal_hits(database, hits, numbers, data_average, is_star, aprox)
+    rotation = pd.DataFrame({'Hits': hits.iloc[0], 'Average_Numbers': data_average.iloc[0], 'Average': average, 'Hits_Needed': m_hits}, index = range(index_start, index_end + 1))
+    rotation['Difference'] = rotation['Hits'] - rotation['Hits_Needed']
+    return rotation
+
+def get_rotations(database, hits, numbers, data_average, is_star=False):
+    aprox_rotations = []
+    exact_rotations = []
+    index_ranges = [(1, 25, '_low'), (26, 50, '_high')]
+    
+    for start, end, suffix in index_ranges:
+        # exact rotations
+        exact_rotation = natural_rotation(database, hits, numbers, data_average, start, end, is_star=is_star, aprox=False)
+        exact_rotations.append(exact_rotation)
+        # approx rotations
+        aprox_rotation = natural_rotation(database, hits, numbers, data_average, start, end, is_star=is_star, aprox=True)
+        aprox_rotations.append(aprox_rotation)
+        
+    return tuple(aprox_rotations + exact_rotations)
+
+def combination_count(database, numbers, df_with_numbers):
+    def low_numbers(numbers):
+        return set(range(1,26)).intersection(numbers)
+    
+    def make_list(list_of_numbers, count_type=None):
+        if count_type == 'low_high':
+            return [number in low_numbers(numbers) for number in list_of_numbers]
+        elif count_type == 'odd_even':
+            return [True if number % 2 != 0 else False for number in list_of_numbers]
+        
+    low_high = []
+    odd_even = []
+    for draw in range(len(database)):
+        list_of_numbers = df_with_numbers.loc[draw, :].tolist()
+        count = Counter(make_list(list_of_numbers, count_type='low_high'))
+        low_high.append((count.get(True,0), count.get(False,0)))
+        count = Counter(make_list(list_of_numbers, count_type='odd_even'))
+        odd_even.append((count.get(True,0), count.get(False,0)))
+        
+    return low_high, odd_even
+
+def combination_df(database, low_high_counts, odd_even_counts):
+    COMBINATIONS = [(3,2), (2,3), (1,4), (4,1), (0,5), (5,0)]
+    draws = set(range(0,len(database)))
+    columns_id = ['3/2', '2/3', '1/4', '4/1', '0/5', '5/0']
+    
+    low_high = {}
+    odd_even = {}
+    for i in draws:
+        counts_l_h = {}
+        counts_o_e = {}
+        for combination in COMBINATIONS:
+            count_l_h = sum([1 for j in range(i-9,i+1) if combination[0] == low_high_counts[j][0] and combination[1] == low_high_counts[j][1]])
+            counts_l_h[combination] = count_l_h
+            count_o_e = sum([1 for j in range(i-9,i+1) if combination[0] == odd_even_counts[j][0] and combination[1] == odd_even_counts[j][1]])
+            counts_o_e[combination] = count_o_e
+        low_high[i] = counts_l_h
+        odd_even[i] = counts_o_e
+    
+    low_high = clean_df(pd.DataFrame.from_dict(low_high, orient='index'), columns_id, 'L/H')
+    odd_even = clean_df(pd.DataFrame.from_dict(odd_even, orient='index'), columns_id, 'O/E')
+    
+    return low_high, odd_even
+
+def count_combinations(df, columns, combinations, name):
+    count_dic = {i: {key: 0 for key in combinations} for i in range(1, len(df) - 99)}
+    columns_id = ['3/2', '2/3', '1/4', '4/1', '0/5', '5/0']
+    for i, _ in enumerate(range(1, len(df) - 99)):
+        df_slice = df.iloc[i:i+100]
+        counts = [df_slice[(df_slice[columns[0]] == combination[0]) & (df_slice[columns[1]] == combination[1])][columns[0]].count() for combination in combinations]
+        count_dic[i+1] = dict(zip(combinations, counts))
+        
+    df = clean_df(pd.DataFrame.from_dict(count_dic, orient='index'), columns_id, name)
+    return df

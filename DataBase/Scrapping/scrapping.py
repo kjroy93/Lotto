0455f18f94d6cd6b0346ed9e68aec856953615c1
ojedalatronico <ojedalatronico@gmail.com>
2023-04-15 21:48:36 +0200

    scrapping fixed

--- DataBase/Scrapping/scrapping.py
+++ DataBase/Scrapping/scrapping.py
@@ -1,62 +1,44 @@
-"""Archivo para realizar scrapping de euromillones
-El scrapping se realiza del año 2013 al 2024. Ajustable
-Solo se debe ajustar el año de inicio y fin"""
-
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
 
-def redimensionado(dataframe):
-    """Función para limpieza de data scrapeada.
-    Recibe df con clumnas con numeros del 0 al 11,
-    Se renombran del 0 al 9.
-    Se elimina las dos primeras lineas ya que no cuentan con información"""
-    return dataframe[0].rename(columns={
-        0:"SEM.",
-        1:"SORTEO",
-        2:"DIA",
-        3:"NRO.1",
-        4:"NRO.2",
-        5:"NRO.3",
-        6:"NRO.4",
-        7:"NRO.5",
-        8:"ESTRELLA.1",
-        9:"ESTRELLA.2"
-    }).drop([0,1])
+from time import gmtime
 
-def get_data():
+def euro_scraping():
+    """Return all euromillones data"""
+    
+    assert(requests.get("https://www.euromillones.com.es/").status_code == 200, 'Hay un problema con la página de euromillones')
+    
+    
+    def rename_sorteo(df:pd.DataFrame):
+        return df.rename(columns={
+                    "SORTEO":'Sorteo',
+                    "FECHA":'Dates',
+                    "DIA":'Dates',
+                    "COMBINACION GANADORA":'Nro1',
+                    "COMBINACION GANADORA.1":'Nro2',
+                    "COMBINACION GANADORA.2":'Nro3',
+                    "COMBINACION GANADORA.3":'Nro4',
+                    "COMBINACION GANADORA.4":'Nro5',
+                    "COMBINACION GANADORA.5":'Star_1',
+                    "COMBINACION GANADORA.6":'Star_2',
+                    })
 
-    sorteos= pd.DataFrame()
-
-    for year in range(2004,2013):
-
-        url = f'https://www.euromillones.com.es/historico/resultados-euromillones-{year}.html'
-        r = requests.get(url)
-        bs = BeautifulSoup(r.text)
-        datos = [i.text.split('\n') for i in bs.find_all('tr')]
-        dfs = pd.DataFrame(datos)[2:54].rename(columns={
-                0:"SEM.",
-                1:"SORTEO",
-                2:"DIA",
-                3:"NRO.1",
-                4:"NRO.2",
-                5:"NRO.3",
-                6:"NRO.4",
-                7:"NRO.5",
-                8:"ESTRELLA.1",
-                9:"ESTRELLA.2"
-            }).drop(columns=[10])
-        dfs['DIA'] = dfs['DIA'] + f"-{year}"
-        sorteos= pd.concat([sorteos,dfs])
-
-    for year in range(2012,2024):                                                                                                              
-        url = f'https://www.euromillones.com.es/historico/resultados-euromillones-{year}.html'
-        dfs = pd.read_html(url)
-        dfs = redimensionado(dfs)
-        dfs['DIA'] = dfs['DIA'] + f"-{year}"
-        sorteos= pd.concat([sorteos,dfs])
-    sorteos.drop(columns=[10,11],inplace=True)
-    sorteos.dropna(inplace=True)
-    sorteos.reset_index(inplace=True)
-#     sorteos.to_csv("sorteos.csv",index=False)
-    return sorteos
+    sorteo_euro_millones = pd.DataFrame()
+    for age in range(2004,gmtime().tm_year+1):
+        try:
+            if age < 2009:
+                new_data = rename_sorteo(pd.read_html(f'https://www.euromillones.com.es/historico/resultados-euromillones-{age}.html',header=0)[0].drop(0))
+                sorteo_euro_millones = pd.concat([sorteo_euro_millones,new_data],ignore_index=True)
+            elif age == 2009 or age == 2010:
+                new_data = rename_sorteo(pd.read_html(f'https://www.euromillones.com.es/historico/resultados-euromillones-{age}.html',header=0)[0][:-1 if age == 2009 else -2].drop(0).drop(columns="COMBINACION GANADORA.7"))
+                sorteo_euro_millones = pd.concat([sorteo_euro_millones,new_data],ignore_index=True)
+            elif age == 2018:
+                new_data = rename_sorteo(pd.read_html(f'https://www.euromillones.com.es/historico/resultados-euromillones-{age}.html',header=0)[0].drop([0,18,19]).drop(columns=['SEM.','EL MILLÓN','Unnamed: 11']))
+                sorteo_euro_millones = pd.concat([sorteo_euro_millones,new_data],ignore_index=True)
+            else:
+                new_data = rename_sorteo(pd.read_html(f'https://www.euromillones.com.es/historico/resultados-euromillones-{age}.html',header=0)[0].drop(0).drop(columns=['SEM.'] if age <= 2015 else ['SEM.','EL MILLÓN']))
+                sorteo_euro_millones = pd.concat([sorteo_euro_millones,new_data.dropna()],ignore_index=True)
+        except:
+            print(f'Error en el año: {age}')
+    assert(not sorteo_euro_millones.empty, "El dataframe no ha sido llenado")
+    return sorteo_euro_millones
